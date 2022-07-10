54fa26759d23453a7a6f05739e3b5c5abb927eee
ange1inaxu <106881758+ange1inaxu@users.noreply.github.com>
2022-07-10 17:38:56 +0300

    Update and rename lab (1).py to boolean_sat_solver.py

--- boolean_sat_solver.py
+++ boolean_sat_solver.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""6.009 Lab 5 -- Boolean satisfiability solving"""
+"""Boolean satisfiability solving"""
 
 import sys
 sys.setrecursionlimit(10000)
-# NO ADDITIONAL IMPORTS
 
 def simplify_with_unit_clauses(formula, assignments):
     '''
